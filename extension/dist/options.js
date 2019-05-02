3052cdf23e2697c54290cbe9a72af1b8bbbf8917
WebDEV <app.dev0528@gmail.com>
2019-05-02 03:10:05 -0300

    modified configuration

--- extension/dist/options.js
+++ extension/dist/options.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "d6d87e110e2836590bac"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "392506b82a8265522750"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
